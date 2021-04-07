9930474db869375ba2a29d2a3ce05082ef8b7472
Okankaan <okankaan.cetinkaya@gmail.com>
2021-04-07 23:01:29 +0300

    Entity Framework - Database First Demo

---
+++ EntityFrameworkDatabaseFirstApp(JustConsoleApp)/EntityFrameworkDatabaseFirstApp(JustConsoleApp)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDatabaseFirstApp_JustConsoleApp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var categories = db.Categories;
+            foreach (var cat in categories)
+            {
+                Console.WriteLine(cat.CategoryName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
