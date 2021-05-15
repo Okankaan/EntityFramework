41050b490b18603b977df3026e7637c5fb3ab175
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-16 00:29:11 +0300

    Project name changed. And Migration dlls added.

---
+++ FootballApp_AutomaticMigrationEnabled_false/FootballApp_AutomaticMigrationEnabled=false/FootballDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballApp_AutomaticMigrationEnabled_false
+{
+    public class FootballDbContext:DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public FootballDbContext():base("FootballDbConStr")
+        {
+
+        }
+    }
+}
---
+++ FootballApp_AutomaticMigrationEnabled_false/FootballApp_AutomaticMigrationEnabled=false/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballApp_AutomaticMigrationEnabled_false
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short YearOfFoundation { get; set; }
+    }
+}
