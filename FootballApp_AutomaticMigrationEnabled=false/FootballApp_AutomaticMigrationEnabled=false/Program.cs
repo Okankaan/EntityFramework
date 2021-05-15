24e50520014eefd6215b94279470f0b3cdf2adda
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-16 00:14:57 +0300

    FootballApp_AutomaticMigrationEnabled=false named Project Created. EntityFramework added to my FootballApp project. -Package Maneger Console: --PM> install-package EntityFramework
    *Above command, runned. And Entity Framework dlls are added to my project.

---
+++ FootballApp_AutomaticMigrationEnabled=false/FootballApp_AutomaticMigrationEnabled=false/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballApp_AutomaticMigrationEnabled_false
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
