24b35ee39397f04951b2b7eb1e63b8643091599c
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-15 22:15:25 +0300

    Just FootballApplication_CodeFirst_ConsoleApp Project Created.

---
+++ FootballApplication_CodeFirst(ConsoleApp)/FootballApplication_CodeFirst(ConsoleApp)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballApplication_CodeFirst_ConsoleApp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
