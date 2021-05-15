063e97248a01289e97ce684626d13767deed873b
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-15 22:08:46 +0300

    Single(), SingleOrDefault(), First() and FirstOrDefault() methods using. -Single() method using for only if we are sure DB will return a result. If DB return more than 1 result or DB is not return any product, we get Error.  -SingleOrDefault() method using for only if we are sure DB will return a result. If DB return more than 1 result, we get Error. If there is no any result of DB Table, DB return null param, it is not return Error.  -First() method using for if we are sure DB will return a result or more than 1 result. If there is no any result of DB Table, DB return null param, it is not return Error.  -FirstOrDefault() method using for get First item result of DB Table. With this method, we can not get any Error.

--- EntityFrameworkDatabaseFirstApp(JustConsoleApp)/EntityFrameworkDatabaseFirstApp(JustConsoleApp)/Program.cs
+++ EntityFrameworkDatabaseFirstApp(JustConsoleApp)/EntityFrameworkDatabaseFirstApp(JustConsoleApp)/Program.cs
@@ -11,12 +11,15 @@ namespace EntityFrameworkDatabaseFirstApp_JustConsoleApp_
     {
         static void Main(string[] args)
         {
-            //All() method using for All DB data sets(return true or false)
-            //Any() method using for Any DB data sets(return true or false)
-            //Any() methodu genellikle Login işlemlerinde, böyle bir kullanıcı var mı? şeklinde kullanıyoruz.
+            //Single() method using for only if we are sure DB will return a result. If DB return more than 1 result or DB is not return any product, we get Error. 
+            //SingleOrDefault() method using for only if we are sure DB will return a result. If DB return more than 1 result, we get Error. If there is no any result of DB Table, DB return null param, it is not return Error.
+            //First() method using for if we are sure DB will return a result or more than 1 result. If there is no any result of DB Table, DB return null param, it is not return Error.
+            //FirstOrDefault() method using for get First item result of DB Table. With this method, we can not get any Error.
             NORTHWNDEntities db = new NORTHWNDEntities();
-            //var result = db.Products.All(x=>x.UnitPrice>0); //If UnitPrice > 0 of my All Products return true.
-            var result = db.Products.Any(x => x.UnitPrice > 0); //If UnitPrice > 0 of my Any Products return true.
+            //var result = db.Products.Single(x => x.CategoryID == 1); //This Single() method return us Error. Because DB return more than 1 result.
+            //var result = db.Products.SingleOrDefault(x => x.CategoryID == 1); //This SingleOrDefault() method return us Error. Because DB return more than 1 result.
+            //var result = db.Products.First(x => x.CategoryID == 1); //This First() method return us First data of CategoryID=1. If DB is not return ant result, It get Error.
+            var result = db.Products.FirstOrDefault(x => x.CategoryID == 1); //This FirstOrDefault() method return us First data of CategoryID=1. With this method, we can not get any Error.
             Console.WriteLine(result);
             Console.ReadLine();
         }
