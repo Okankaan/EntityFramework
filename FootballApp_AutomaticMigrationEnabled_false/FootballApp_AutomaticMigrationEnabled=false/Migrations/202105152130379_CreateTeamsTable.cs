833804674576fb381d16fdce43df458111ca28a3
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-16 00:31:50 +0300

    Migration added: -PM> add-migration CreateTeamsTable

---
+++ FootballApp_AutomaticMigrationEnabled_false/FootballApp_AutomaticMigrationEnabled=false/Migrations/202105152130379_CreateTeamsTable.cs
@@ -0,0 +1,27 @@
+﻿namespace FootballApp_AutomaticMigrationEnabled_false.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTeamsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        YearOfFoundation = c.Short(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Teams");
+        }
+    }
+}
