f59042a8093e36f358a2aefca40ab9e821a5f8d5
Okankaan <okankaan.cetinkaya@gmail.com>
2021-05-16 04:03:21 +0300

    Column Added to Countries Table. Population column name added to Country.cs class and get migration with following command lines: -PM> add-migration CreatePopulationColumnInCountriesTable -PM> update-database

--- FootballApp_AutomaticMigrationEnabled_false/FootballApp_AutomaticMigrationEnabled=false/Country.cs
+++ FootballApp_AutomaticMigrationEnabled_false/FootballApp_AutomaticMigrationEnabled=false/Country.cs
@@ -10,6 +10,7 @@ namespace FootballApp_AutomaticMigrationEnabled_false
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Population { get; set; }
         public ICollection<Team> Teams { get; set; }
     }
 }
---
+++ FootballApp_AutomaticMigrationEnabled_false/FootballApp_AutomaticMigrationEnabled=false/Migrations/202105160101017_CreatePopulationColumnInCountriesTable.cs
@@ -0,0 +1,18 @@
+﻿namespace FootballApp_AutomaticMigrationEnabled_false.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatePopulationColumnInCountriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Countries", "Population", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Countries", "Population");
+        }
+    }
+}
